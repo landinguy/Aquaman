ae4b368074059d7cce028c0323f77d6e04d00635
landinguy <lo29573528@qq.com>
2019-03-14 22:12:21 +0800

    update

--- src/components/school/Info.vue
+++ src/components/school/Info.vue
@@ -17,10 +17,20 @@
       </Card>
       <Card style="margin: 16px 0">
         <p slot="title">其他信息</p>
-        <div class="line"><label>英文名</label><span>{{data.englishName}}</span></div>
-        <div class="line"><label>校训</label><span>{{data.motto}}</span></div>
-        <div class="line"><label>学校地址</label><span>{{data.address}}</span></div>
-        <div class="line"><label>学校官网</label><span><a href="http://www.pku.edu.cn">{{data.website}}</a></span></div>
+        <Row>
+          <Col span="12">
+            <div class="line"><label>校名</label><span>{{data.name}}</span></div>
+            <div class="line"><label>英文名</label><span>{{data.englishName}}</span></div>
+            <div class="line"><label>校训</label><span>{{data.motto}}</span></div>
+            <div class="line"><label>学校类型</label><span>{{data.schoolTypeCode==1?'公立学校':'私立学校'}}</span></div>
+          </Col>
+          <Col span="12">
+            <div class="line"><label>所属区域</label><span>{{data.area}}</span></div>
+            <div class="line"><label>管理单位</label><span>{{data.competentDepartment}}</span></div>
+            <div class="line"><label>学校地址</label><span>{{data.address}}</span></div>
+            <div class="line"><label>学校官网</label><span><a href="http://www.pku.edu.cn">{{data.website}}</a></span></div>
+          </Col>
+        </Row>
       </Card>
       <div class="btn">
         <Button type="ghost" shape="circle" @click="edit()" style="width: 80px">编辑</Button>
--- src/mock/test.js
+++ src/mock/test.js
@@ -12,6 +12,9 @@ Mock.mock("/api/management/getSchool", "post", {
     website: 'http://www.pku.edu.cn',
     badge: 'https://gss0.bdstatic.com/-4o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike150%2C5%2C5%2C150%2C50/sign=ea9f882693510fb36c147fc5b85aa3f0/b21bb051f81986183a28291b47ed2e738ad4e6c6.jpg',
     description: '北京大学（Peking University），简称“北大”，由中华人民共和国教育部直属，中央直管副部级建制，位列“211工程”、“985工程”、“世界一流大学和一流学科”，入选“基础学科拔尖学生培养试验计划”、“高等学校创新能力提升计划”、“高等学校学科创新引智计划”，为九校联盟、中国大学校长联谊会、京港大学联盟、亚洲大学联盟、东亚研究型大学协会、国际研究型大学联盟、环太平洋大学联盟、东亚四大学论坛、国际公立大学论坛、中俄综合性大学联盟成员。',
+    area: '北京市海淀区',
+    competentDepartment: '北京市教育局',
+    schoolTypeCode: 1,
   }
 });
 
