46cb2a70979b63e02595fe872c105f94a5b8378a
landinguy <1029573528@qq.com>
2020-04-26 13:55:32 +0800

    update

--- src/libs/url.js
+++ src/libs/url.js
@@ -1,6 +1,6 @@
 var url = {
-  // base: "http://127.0.0.1:8888/examination/"
-  base: "http://49.234.19.183:8888/examination/"
+  base: "http://127.0.0.1:8888/examination/"
+  // base: "http://49.234.19.183:8888/examination/"
 }
 
 export default url;
